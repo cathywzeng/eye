54515f01b630007b3f54e38c537be8ce5b875dbe
cathywzeng <38617454+cathywzeng@users.noreply.github.com>
2026-07-07 23:58:35 +0800

    add sigmoid

--- train_dual_head.py
+++ train_dual_head.py
@@ -184,40 +184,17 @@ def train_model():
             # 预测框中心
             pred_box_center_x = (pred_reg[:, 0] + pred_reg[:, 2]) / 2.0
             pred_box_center_y = (pred_reg[:, 1] + pred_reg[:, 3]) / 2.0
+
+             # 【核心修复】强制将预测框限制在 0-1 之间，防止模型画极小的框偷懒！
+            pred_reg_sigmoid = torch.sigmoid(pred_reg)
             
-            # 1. 回归头 Loss
-            loss_reg = criterion_reg(pred_reg, boxes)
+            # 1. 回归头 Loss (必须用加了 Sigmoid 的输出去和标签算 Loss)
+            loss_reg = criterion_reg(pred_reg_sigmoid, boxes)
             
             # 2. 热力图头 Loss
             loss_heatmap = criterion_heatmap(pred_heatmap, heatmaps)
-            
-            # # 3. 【核心新增】一致性约束 Loss (强迫热力图中心和框的中心对齐！)
-            # b, c, h, w = pred_heatmap.shape
-            # y_indices = torch.arange(h, device=DEVICE, dtype=torch.float32).view(1, 1, h) / h
-            # x_indices = torch.arange(w, device=DEVICE, dtype=torch.float32).view(1, 1, w) / w
-            
-            # # 计算热力图的加权平均坐标 (Center of Mass)
-            # sum_heatmap = pred_heatmap.sum(dim=[2, 3], keepdim=True) + 1e-6  # 保持维度 [16, 1, 1, 1]
-            
-            # # 【核心修复】使用 keepdim=True，确保算出来的重心形状是 [16, 1, 1]
-            # center_x = (pred_heatmap * x_indices).sum(dim=[2, 3], keepdim=True) / sum_heatmap
-            # center_y = (pred_heatmap * y_indices).sum(dim=[2, 3], keepdim=True) / sum_heatmap
-            
-            # # 把 [16, 1, 1] 压平为 [16, 1]
-            # center_x = center_x.view(b, 1)
-            # center_y = center_y.view(b, 1)
-            
-            # # 预测框中心 (形状已经是 [16, 1])
-            # pred_box_center_x = ((pred_reg[:, 0] + pred_reg[:, 2]) / 2.0).view(b, 1)
-            # pred_box_center_y = ((pred_reg[:, 1] + pred_reg[:, 3]) / 2.0).view(b, 1)
-            
-            # # 完美对齐的 MSE Loss
-            # loss_consistency = nn.MSELoss()(
-            #     torch.cat([center_x, center_y], dim=1),  # 形状 [16, 2]
-            #     torch.cat([pred_box_center_x, pred_box_center_y], dim=1)  # 形状 [16, 2]
-            # )
-            
-            # 4. 总 Loss = 框的损失 + 热力图的损失 + 对齐的损失
+
+            # 4. 总 Loss = 框的损失 + 热力图的损失 
             total_loss = loss_reg + 1.0 * loss_heatmap
             
             total_loss.backward()
